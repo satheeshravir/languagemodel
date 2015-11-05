d5d220e9c94fc47d9890afeec523315cee5acf18
satheeshravir <satheeshravir@gmail.com>
2015-11-04 18:52:23 -0800

    Modified the test pipe

--- TestPipe.py
+++ TestPipe.py
@@ -4,7 +4,7 @@ import subprocess
 
 
 
-#subprocess.Popen(["python", "WithoutTokenizer.py", "demo-script-linked-q-a.txt", "en-ta.txt"])
-#subprocess.Popen(["python", "WithTokenizer.py", "demo-script-linked-q-a.txt", "en-ta.txt"])
-#subprocess.Popen(["python", "WithoutTokenizer.py", "demo-script-linked-q-a.txt", "en-test.txt"])
-subprocess.Popen(["python", "WithTokenizer.py", "demo-script-linked-q-a.txt", "en-test.txt"])
+subprocess.Popen(["python", "WithoutTokenizer.py", "tamil_corpus.txt", "test-ta.txt"])
+#subprocess.Popen(["python", "WithTokenizer.py", "tamil_corpus.txt", "test-ta.txt"])
+#subprocess.Popen(["python", "WithoutTokenizer.py", "demo-script-linked-q-a.txt", "test-en.txt"])
+#subprocess.Popen(["python", "WithTokenizer.py", "demo-script-linked-q-a.txt", "en-test.txt"])
